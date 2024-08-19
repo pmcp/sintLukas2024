4dbc48eed41bdd828b6198229079a2eda1037b3f
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-08-19 14:49:53 +0200

    added pages

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+<!--  <ContentList path="/" v-slot="{ list }">-->
+<!--    <div v-for="article in list" :key="article._path">-->
+<!--      <ULink :to="article._path">{{ article.title }}</ULink>-->
+<!--      <p>{{ article.description }}</p>-->
+<!--    </div>-->
+<!--  </ContentList>-->
+</template>
+
+<!--<script setup>-->
+
+<!--useHead({-->
+<!--  script: [-->
+<!--    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },-->
+<!--  ],-->
+<!--});-->
+
+<!--</script>-->
