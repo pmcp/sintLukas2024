f91ad6d49c3057539565d7a5a640cf1b8c1088b7
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-08-19 14:48:57 +0200

    added configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile: ['@egjs/vue-grid']
+  },
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
-  routeRules: {
-    '/': { prerender: true }
+  extends: ['@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxt/image',
+    'nuxt-mapbox'
+  ],
+  colorMode: {
+    preference: 'light'
+  },
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoicG1jcCIsImEiOiJjbHduc3hkMGExdzNwMmpteWh2M3c3cHZ3In0.4edOOiqp_E93TDvm-1BxCQ'
+  },
+  css: ['~/assets/css/main.css'],
+  ui: {
+    safelistColors: ['black', 'primary', 'secondary']
+  },
+  image: {
+    provider: 'netlify'
   }
+  // content: {
+  //   navigation: {
+  //     fields: ['title']
+  //   }
+  // }
+  // content: {
+  //   markdown: {
+  //     tags: { dispatch: 'dispatch'}
+  //   }
+  // }
 })
