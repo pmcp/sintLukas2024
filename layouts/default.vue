df2fe277d63d7cc212baea3c56110638b70cc39b
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-08-19 14:50:12 +0200

    added layout

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <main-header />
+    <UPage>
+      <UPageBody :ui="{ wrapper: 'mt-0'}">
+        <slot />
+      </UPageBody>
+    </UPage>
+    <main-footer />
+  </div>
+</template>
