5cbfe02f6dbe29df581eecd2275741064c01a5c4
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-08-22 10:51:12 +0200

    added download icon

--- components/elements/button.vue
+++ components/elements/button.vue
@@ -1,8 +1,18 @@
 <template>
   <!--  TODO: add download -->
-  <UButton class="hover:bg-primary-100" v-if="style === 0" color="black" variant="outline" :ui="{ rounded: 'rounded-none' }"><slot /></UButton>
-  <UButton v-if="style === 1"  color="black" :ui="{ rounded: 'rounded-none'}"><slot /></UButton>
-  <UButton v-if="style === 2"  color="primary" :ui="{ rounded: 'rounded-none' }"><slot /></UButton>
+
+  <UButton class="hover:bg-primary-100" v-if="style === 0" color="black" variant="outline" :ui="{ rounded: 'rounded-none' }">
+    <svg-icon-download v-if="download" class=" w-4 h-4"/>
+    <slot />
+  </UButton>
+  <UButton v-if="style === 1"  color="black" :ui="{ rounded: 'rounded-none'}">
+    <svg-icon-download v-if="download" class=" w-4 h-4"/>
+    <slot />
+  </UButton>
+  <UButton v-if="style === 2"  color="primary" :ui="{ rounded: 'rounded-none' }">
+    <svg-icon-download v-if="download" class=" w-4 h-4"/>
+    <slot />
+  </UButton>
 </template>
 
 <script>
---
+++ components/svg/icon_download.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg  viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 14.6667L12 14.6667" stroke="currentColor" stroke-linecap="round"/>
+    <path d="M4.6665 8L7.99984 11.3333" stroke="currentColor" stroke-linecap="round"/>
+    <path d="M8 11.3334L11.3333 8.00004" stroke="currentColor" stroke-linecap="round"/>
+    <path d="M8 11.3334L8 1.33337" stroke="currentColor" stroke-linecap="round"/>
+  </svg>
+
+</template>
