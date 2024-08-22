ec42dc3bfa1867f0f543f6618c383596896c725d
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-08-22 09:43:02 +0200

    added bg style flow better

---
+++ components/elements/background.vue
@@ -0,0 +1,10 @@
+<template>
+  <div v-if="data.style === 1 " class="background-grid" style="width: 300vw; position: absolute; left: -100vw; height: 99999px;" />
+  <div v-if="data.style === 2 " class="bg-white" style="width: 300vw; position: absolute; left: -100vw; height: 99999px;" />
+</template>
+<script setup>
+const props = defineProps({
+  data: Object,
+  type: String
+});
+</script>
--- components/elements/container.vue
+++ components/elements/container.vue
@@ -2,31 +2,16 @@
   <div class="mt-6">
     <UPageGrid :ui="{wrapper: 'grid grid-cols-1 xl:grid-cols-2 gap-8'}">
       <div v-for="(element, key) in elements" :key="`el${key}`" class="pmcp-container" :class="getContainerClasses(element)">
-        <!-- BACKGROUND STYLE -->
-        <div v-if="element.type === 'bg' && element.style === 1 " class="background-grid" style="width: 300vw; position: absolute; left: -100vw; height: 99999px;">
-          {{  element }}
-        </div>
-        <div v-if="element.type === 'bg' && element.style === 2 " class="bg-white" style="width: 300vw; position: absolute; left: -100vw; height: 99999px;">
-          {{  element }}
-        </div>
-        <!-- TITLE-->
-        <h2 v-if="element.title && element.type !== 'banner'" class="text-md uppercase font-bold pt-2 pb-4">{{ element.title}}</h2>
-        <!-- BANNER -->
-        <elements-banner v-if="element.type === 'banner'" :data="element" />
-        <!-- TEXT -->
-        <elements-text v-if="element.type === 'text'" :text="element.markdown" :side1="element.side1" :side2="element.side2" />
-        <!-- IMAGES -->
-        <elements-images v-if="element.type === 'images'" :images="element.image" :layout="element.layout" />
-        <!-- LOCATION -->
-        <elements-locations v-if="element.type === 'locations'" :locations="element.location" />
-        <!-- TEACHERS -->
-        <elements-teachers v-if="element.type === 'teachers'" :teachers="element.teacher" />
-        <!-- COURSES -->
-        <elements-courses v-if="element.type === 'ateliers'" :categories="element.category" />
-        <!-- NEWS -->
-        <elements-news v-if="element.type === 'news'" :articles="element.articles"/>
-        <!-- CATEGORIES -->
-        <elements-categories v-if="element.type === 'categories'" :categories="element.categories"/>
+        <!-- BACKGROUND --> <elements-background v-if="element.type === 'bg'" :data="element" />
+        <!-- TITLE-->       <h2 v-if="element.title && element.type !== 'banner'" class="text-md uppercase font-bold pt-2 pb-4">{{ element.title}}</h2>
+        <!-- BANNER -->     <elements-banner v-if="element.type === 'banner'" :data="element" />
+        <!-- TEXT -->       <elements-text v-if="element.type === 'text'" :text="element.markdown" :side1="element.side1" :side2="element.side2" />
+        <!-- IMAGES -->     <elements-images v-if="element.type === 'images'" :images="element.image" :layout="element.layout" />
+        <!-- LOCATION -->   <elements-locations v-if="element.type === 'locations'" :locations="element.location" />
+        <!-- TEACHERS -->   <elements-teachers v-if="element.type === 'teachers'" :teachers="element.teacher" />
+        <!-- COURSES -->    <elements-courses v-if="element.type === 'ateliers'" :categories="element.category" />
+        <!-- NEWS -->       <elements-news v-if="element.type === 'news'" :articles="element.articles"/>
+        <!-- CATEGORIES --> <elements-categories v-if="element.type === 'categories'" :categories="element.categories"/>
       </div>
     </UPageGrid>
   </div>
