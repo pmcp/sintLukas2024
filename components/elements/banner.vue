295c12674a9d1a8cc1afd867e7bfcc4a1a43c82a
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-08-22 15:21:06 +0200

    bigger banner

--- components/elements/banner.vue
+++ components/elements/banner.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full pb-24 pt-8 relative ">
-    <div class="w-full h-full absolute object-cover top-0 left-0">
+    <div class="w-full h-full absolute object-cover top-0 left-0" style="width: 120%;left: -10%;">
       <img v-if="data.image" class="z-0 absolute object-cover w-full h-full	 top-0 left-0" :src="data.image" :alt="data.alt">
     </div>
     <div class="relative text-3xl max-w-md h-auto whitespace-pre-line	">
