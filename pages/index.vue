462bf99eee1f4c01cb292a83862109ec5d8605bc
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-08-25 20:07:03 +0200

    clean up 7

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLayout>
-    <elements-container v-if="data" class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl " :elements="data.elements"/>
+<!--    <elements-container v-if="data" class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl " :elements="data.elements"/>-->
   </NuxtLayout>
 </template>
 
@@ -9,8 +9,5 @@
 <script setup>
 // Get the Homepage Id
 const { data: page } = await useAsyncData('homepage', () => queryContent('site/homepage').findOne())
-// Get the Homepage Content
-const { data } = await useAsyncData('home', () => queryContent('pages')
-    .where({ id: page.value.page })
-    .findOne())
+const { data } = await useAsyncData('home', () => queryContent(`pages/nl/${page.value.page}`).findOne())
 </script>
