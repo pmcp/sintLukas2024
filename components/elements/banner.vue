f600da562e197384141099570f9ab4531ddf489f
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-08-28 11:41:16 +0200

    fixed image hidration issue 3

--- components/elements/banner.vue
+++ components/elements/banner.vue
@@ -2,8 +2,6 @@
   <div class="w-full pb-24 pt-8 relative ">
     <div v-if="data.image" class="w-full h-full absolute object-cover top-0 left-0" style="width: 120%;left: -10%;">
       <elements-image transparent :image="data.image" class="col-span-1 h-full w-full object-cover object-center hover:object-scale-down"/>
-
-      <img  class="z-0 absolute object-cover w-full h-full	 top-0 left-0" :src="data.image" :alt="data.alt">
     </div>
     <div class="relative text-3xl max-w-md h-auto whitespace-pre-line	">
       {{ data.title }}
