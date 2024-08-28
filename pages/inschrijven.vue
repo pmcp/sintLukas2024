3e867fa4a779e2d709e5609086f829571d3e0dd6
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-08-28 17:29:55 +0200

    fixed banner for inschrijvingen

--- pages/inschrijven.vue
+++ pages/inschrijven.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout class="relative" v-if="data">
-    <elements-container class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl ">
       <elements-banner :data="{ title: '', image: data.images }" class="col-span-1 md:col-span-2 h-48"/>
+    <elements-container class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl ">
       <div class="flex flex-col col-span-full md:col-span-2 gap-16">
         <h1 class="text-4xl pb-0">Inschrijven</h1>
         <div class="flex items-center ">
