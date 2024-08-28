bd58f7d6b3e74fdfd08d7d9a7de45217a3a3e018
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-08-28 16:26:11 +0200

    col span fix aanbod

--- pages/aanbod/index.vue
+++ pages/aanbod/index.vue
@@ -2,8 +2,8 @@
   <nuxt-layout grid>
     <elements-container class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
       <elements-banner :data="{ title: 'Vind jouw atelier. \nOntdek onze extra muros projecten.'}" class="col-span-2"/>
-      <div class="grid grid-cols-1 md:grid-cols-2 w-full items-end gap-8">
-        <nuxt-link v-for="c in courses" :key="`courses_${c.cat.id}`" :to="`/aanbod/${c.cat.id}/${c.id}`" class="grow mb-2">
+      <div class="w-full col-span-2 md:col-span-1 grid grid-cols-1 md:grid-cols-2 w-full items-end gap-8">
+        <nuxt-link v-for="c in courses" :key="`courses_${c.cat.id}`" :to="`/aanbod/${c.cat.id}/${c.id}`" class="w-full mb-2">
           <elements-course :course="c" class="grow mb-2"/>
         </nuxt-link>
       </div>
