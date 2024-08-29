ff182c0c7dc9e087ce756d4625b41ec2e7de0b25
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-08-29 17:05:25 +0200

    changed color button

--- components/elements/button.vue
+++ components/elements/button.vue
@@ -1,6 +1,7 @@
 <template>
   <UButton :to="url" :download="download.length > 0" :target="download.length > 0 ? '_blank' : '_self'" class="hover:bg-primary-100 w-fit" v-if="flavor === 0" color="black" variant="outline" :ui="{ rounded: 'rounded-none'}">
     <svg-icon-download v-if="download" class=" w-4 h-4"/>
+
     <slot />
 
   </UButton>
@@ -10,7 +11,12 @@
     </template>
     <slot />
   </UButton>
-  <UButton :to="url" :download="download.length > 0" :target="download.length > 0 ? '_blank' : '_self'" v-if="flavor === 2"  color="primary" :ui="{ rounded: 'rounded-none' }" class="w-fit">
+  <UButton :to="url" :download="download.length > 0" :target="download.length > 0 ? '_blank' : '_self'" v-if="flavor === 2" color="my" :ui="{ rounded: 'rounded-none',  color: {
+    my: {
+      solid: 'shadow-none ring-none text-black font-bold bg-primary-300 hover:bg-primary-400 disabled:bg-white dark:bg-gray-900 dark:hover:bg-gray-800/50 dark:disabled:bg-gray-900 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400',
+      ghost: 'text-gray-900 dark:text-white hover:bg-white dark:hover:bg-gray-900 focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400',
+    }
+}}" class="w-fit">
     <template #leading>
       <svg-icon-download v-if="download" class=" w-4 h-4"/>
     </template>
