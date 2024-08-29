9d5ed372e72954a478846fd497fedc7bcf626447
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-08-29 16:59:18 +0200

    grid in banner

--- components/elements/banner.vue
+++ components/elements/banner.vue
@@ -1,25 +1,30 @@
 <template>
-  <div class="w-full relative">
+  <div class="w-full relative ">
 
     <nuxtImg v-if="image.cover"
-        class="h-full w-full object-cover object-center "
+        class="h-full w-full object-cover object-center relative z-20"
         :src="image.cover"
         :alt="image.description"
     />
     <div v-else class="h-full w-full">
       </div>
-    <div class="absolute h-full w-full pt-16 top-0">
-      <div class=" text-3xl h-auto whitespace-pre-line mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
+    <div class="absolute z-30 h-full w-full pt-16 top-0">
+      <div class=" text-4xl leading-relaxed h-auto whitespace-pre-line mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
         {{ data.title }}
         </div>
     </div>
+    <div v-if="grid" class="z-10 absolute h-full w-full top-0 left-0 background-grid"></div>
   </div>
 </template>
 
 <script setup>
 
 const props = defineProps({
-  data: Object
+  data: Object,
+  grid: {
+    type: Boolean,
+    default: false
+  }
 });
 
 let image = {}
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <NuxtLayout class="relative">
+  <NuxtLayout class="relative ">
     <!-- BLOCK: BANNER-->
-    <elements-banner class="mb-16 h-80" key="banner-home" :data="{ title: page.block_banner.title, image: page.block_banner.image }"/>
+    <elements-banner class="mb-16 h-80 " grid key="banner-home" :data="{ title: page.block_banner.title, image: page.block_banner.image }"/>
 
 
     <div class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
@@ -17,20 +17,17 @@
           <div class="h-full bg-black-100 w-full"></div>
 
           <!-- BLOCK: AANBOD-->
-          <div v-if="page.block_courses" class="">
+          <div v-if="page.block_courses" class="flex flex-col gap-4 col-span-1">
             <span class="uppercase text-sm font-bold">Aanbod</span>
             <div class="prose-xl">{{ page.block_courses.title }}</div>
-            <elements-categories class="col-span-1"/>
+            <elements-categories class="col-span-1 mt-8"/>
           </div>
+        </div>
 
-
-
-
-</div>
 
         <!-- BLOCK: ABOUT-->
         <template v-if="page.block_about">
-          <div class="grid grid-cols-2 gap-8 col-span-1 col-span-1 md:col-span-2">
+          <div class="grid grid-cols-2 gap-16 col-span-1 col-span-1 md:col-span-2">
             <elements-image :image="page.block_about.image1" :key="page.block_about.image1" />
             <elements-image :image="page.block_about.image2" :key="page.block_about.image2"/>
           </div>
@@ -38,7 +35,7 @@
           <div class="flex flex-col gap-2 col-span-1">
             <span class="uppercase text-sm font-bold">{{ page.block_about.label }}</span>
             <span class="text-2xl">{{ page.block_about.title }}</span>
-            <span class="text-sm">{{ page.block_about.text }}</span>
+            <span class="prose prose-sm">{{ page.block_about.text }}</span>
           </div>
           <elements-image :image="page.block_about.image3" :key="page.block_about.image3" class="col-span-1  h-fit"/>
         </template>
