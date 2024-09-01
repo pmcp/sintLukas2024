6603c53887ec4ae0900f2e26448c156d6cf9d999
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-09-01 23:45:22 +0200

    mobile layout fix 6

--- pages/inschrijven.vue
+++ pages/inschrijven.vue
@@ -2,9 +2,9 @@
   <NuxtLayout class="relative" v-if="data">
       <elements-banner :data="{ title: '', image: data.images }" class="mb-12 md:mb-16 h-60"/>
     <elements-container class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl ">
-      <div class="flex flex-col col-span-full md:col-span-2 gap-16">
+      <div class="flex flex-col col-span-full md:col-span-2 gap-8 md:gap-16">
         <h1 class="text-4xl pb-0">Inschrijven</h1>
-        <div class="flex items-center ">
+        <div class="flex flex-col md:flex-row md:justify-between md:items-center gap-8">
           <div class="grow">
             <elements-markdown :markdownString="data.text1" class="prose-sm lg:prose-sm max-w-prose	"/>
           </div>
@@ -17,7 +17,7 @@
         </div>
         <div class="bg-black-100 w-full col-span-full" style="height:1px" />
       </div>
-      <div class="flex flex-col md:flex-row col-span-1 md:col-span-2 mt-8 gap-16">
+      <div class="flex flex-col md:flex-row col-span-1 md:col-span-2 md:mt-8 gap-16 md:gap-">
         <div class="grid grid-cols-1 gap-16">
             <elements-markdown :markdownString="data.text2" class="prose-sm lg:prose-sm max-w-prose	"/>
             <div class="bg-black-100 w-full col-span-full" style="height:1px" />
