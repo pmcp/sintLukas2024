07c7b40577679634a26420b4e82076b4e674da80
Maarten Lauwaert <github@maartenlauwaert.eu>
2024-09-19 16:03:31 +0200

    fix for person image

--- components/elements/person.vue
+++ components/elements/person.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="flex flex-col w-full" v-if="thePerson">
+
+  <div class="flex flex-col w-full h-full" v-if="thePerson">
     <div class="relative">
-      <nuxtImg v-if="thePerson.image" class="w-full group-hover:scale-110 transition-all ease-in duration-200 grayscale group-hover:grayscale-0 transition-all object-cover	bg-white" :src="data.image" />
-      <USkeleton class="h-32 w-full ratio-square" :ui="{
+      <nuxtImg v-if="thePerson.image" class="w-full group-hover:scale-110 transition-all ease-in duration-200 grayscale group-hover:grayscale-0 transition-all object-cover	bg-white" :src="thePerson.image" />
+      <USkeleton v-else class="h-32 w-full ratio-square" :ui="{
   base: 'animate-none',
   background: 'bg-primary-200',
   rounded: 'rounded-none',
 }" />
     </div>
-    <div class="bg-white px-4 py-2 p-2 sm:px-2 md:py-4 w-full h-full">
+    <div class="bg-white px-4 py-2 p-2 sm:px-2 md:py-4 h-full">
       <span class="text-sm font-bold">{{ thePerson.firstName }} {{  thePerson.lastName }}</span>
     </div>
 </div>
