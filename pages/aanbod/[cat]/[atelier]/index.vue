a8c4d229ee356956ce9ccd2d183f29969b22cfd0
pmcp <github@maartenlauwaert.eu>
2024-10-04 11:40:14 +0200

    fix aanbod 5

--- pages/aanbod/[cat]/[atelier]/index.vue
+++ pages/aanbod/[cat]/[atelier]/index.vue
@@ -13,7 +13,7 @@ const sanitize = (str) => {
 const getAanbod = async () => {
   // Get the category
   // For some reason I can't get it working with "where", so I'm brute forcing it: get all categories, then find the one I want
-  const { data: cat } = await useAsyncData('home', () => queryContent('aanbod', 'nl')
+  const { data: cat } = await useAsyncData('ateliersCat', () => queryContent('aanbod', 'nl')
       .find()
   )
   console.log('cat', cat.value)
