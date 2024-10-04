c4b6df4f1734a1ade9f5594efa8272e0b14e9d30
pmcp <github@maartenlauwaert.eu>
2024-10-04 11:20:22 +0200

    fix aanbod 2

--- pages/aanbod/[cat]/[atelier]/index.vue
+++ pages/aanbod/[cat]/[atelier]/index.vue
@@ -4,21 +4,23 @@
 
 <script setup>
 const route = useRoute()
-// Get the category
-// For some reason I can't get it working with "where", so I'm brute forcing it: get all categories, then find the one I want
-const { data: cat } = await useAsyncData('home', () => queryContent('aanbod', 'nl')
-    // .where({ id: route.params.cat })
-    .find()
-)
 
 // Reverse find the atelier, by finding the category, then the atelier
 const sanitize = (str) => {
   return str.replaceAll(' ', '-').replaceAll('/', '-').replaceAll('&', '-').replaceAll('?', '-').toLocaleLowerCase()
 }
 
-if(cat) {
+const getAanbod = async () => {
+  // Get the category
+  // For some reason I can't get it working with "where", so I'm brute forcing it: get all categories, then find the one I want
+  const { data: cat } = await useAsyncData('home', () => queryContent('aanbod', 'nl')
+      .find()
+  )
   const theCat = cat.value.find(x => sanitize(x.nl) === route.params.cat)
   const atelier = theCat.ateliers.find(x => sanitize(x.title) === route.params.atelier)
+
 }
+
+getAanbod()
 
 </script>
