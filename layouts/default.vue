8e9cba1bc13dae2d22e2cdc57f2f68da680bc692
PMCP <github@maartenlauwaert.eu>
2025-04-30 16:52:47 +0200

    fix white space

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="relative overflow-clip w-full">
     <main-header class="z-40"/>
-    <UPage :class="!aanbod ? 'pb-20' : ''">
+
+    <UPage :class="aanbod ? '' : 'pb-20'">
       <UPageBody :ui="{ wrapper: 'mt-0 pb-0'}">
         <slot />
       </UPageBody>
