12fadc6c1c4dc4bca3212a165457093bbaba6207
PMCP <github@maartenlauwaert.eu>
2025-05-01 11:05:06 +0200

    border fix for sidebar

--- components/pages/atelier.vue
+++ components/pages/atelier.vue
@@ -9,8 +9,8 @@
         </div>
         <div class="grid md:grid-cols-2 col-span-full gap-4 md:gap-0">
           <elements-markdown :markdownString="myData.markdown" class="col-span-1 prose pr-0 md:pr-8 md:pt-4"/>
-          <div class="col-span-1 grid grid-cols-1 lg:grid-cols-3 h-fit">
-            <elements-markdown v-if="myData.side1" :markdownString="myData.side1" class="h-fit pr-2 col-span-1 prose border-l-0 md:border-l md:pl-4 pmcp-prose_sidebar"/>
+          <div class="col-span-1 grid grid-cols-1 lg:grid-cols-3 h-fit border-l-0 md:border-l">
+            <elements-markdown v-if="myData.side1" :markdownString="myData.side1" class="h-fit pr-2 col-span-1 prose  md:pl-4 pmcp-prose_sidebar"/>
             <elements-markdown v-if="myData.side2" :markdownString="myData.side2" class="h-full col-span-2 prose border-l-0 md:border-l md:pl-4 pmcp-prose_sidebar pt-4 md:pt-0"/>
           </div>
 
